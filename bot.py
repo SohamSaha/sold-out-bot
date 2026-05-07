e3b44e115c5d6f09fe3912ef27aef2e0c2760de7
Soham Saha <sohamsaha@Sohams-MacBook-Air.local>
2026-05-07 16:58:47 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+import json
+import aiohttp
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import tasks
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+NOTIFY_USER_ID = os.getenv("NOTIFY_USER_ID")
+
+with open("config.json") as f:
+    config = json.load(f)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@tasks.loop(seconds=60)
+async def check_stock():
+    item = config["items"][0]
+    url = f"{item['base_shop_url']}/products.json"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            data = await resp.json()
+
+    products = data["products"]
+    for p in products:
+        p.get("handle")
+        if p["handle"] == item["product_handle"]:
+            variants = p["variants"]
+    
+    for v in variants:
+        in_stock = any(v.get("available") for v in variants)
+        if (in_stock) and (item["notified"] == False):
+            user = await client.fetch_user(NOTIFY_USER_ID)
+            await user.send(f"Item is back in stock! {item['product_url']}")
+            config["items"][0]["notified"] = True
+            with open('config.json', 'w') as f:
+                json.dump(config, f, indent=4)
+
+@client.event
+async def on_ready():
+    print("Connected to Discord!")
+    check_stock.start()
+
+client.run(DISCORD_TOKEN)
